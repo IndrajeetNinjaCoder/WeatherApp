f0de041408f3df6c39fa3b6c0cf4dc310ad46028
IndrajeetNinjaCoder <2003indrajeet@gmail.com>
2025-01-30 15:09:48 +0530

    first commit

---
+++ WeatherApp/ViewModel/ViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ViewModel.swift
+//  WeatherApp
+//
+//  Created by test on 30/01/25.
+//
+
+import Foundation
+
+// url: "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=1b40a6dc6666b5b9f884b3173846e31a&units=metric"
+
+/* url: "https://api.openweathermap.org/data/2.5/weather?q={city}&appid=1b40a6dc6666b5b9f884b3173846e31a&units=metric"
+ */
+
+
+class ViewModel: ObservableObject {
+    @Published var WeatherData: Model?
+    
+    
+    func getData(city: String) async {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=1b40a6dc6666b5b9f884b3173846e31a&units=metric") else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url);
+            if let decodedResponse = try? JSONDecoder().decode(Model.self, from: data){
+                DispatchQueue.main.async {
+                    self.WeatherData = decodedResponse
+                }
+            }
+            
+            
+            
+        } catch {
+            print("Invalid Response: \(error.localizedDescription)")
+        }
+    }
+    
+}
---
+++ WeatherApp/Views/ContentView.swift
@@ -0,0 +1,208 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by test on 30/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    @State var city: String = ""
+    var body: some View {
+        ZStack {
+            
+//            LinearGradient(
+//                gradient: Gradient(colors: [Color("LightBlue"), .blue]),
+//                startPoint: .topLeading,
+//                endPoint: .bottomTrailing
+//            )
+//                .ignoresSafeArea()
+            
+            
+            Image("bg1")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                SearchView(viewModel: viewModel, city: $city)
+                
+                Spacer()
+                
+                TempView(viewModel: viewModel)
+                
+                Spacer()
+                
+                BottomView(viewModel: viewModel)
+                
+                Spacer()
+                
+            }
+            .foregroundColor(.white)
+            .padding()
+            .task {
+                await viewModel.getData(city: "Delhi")
+            }
+        }
+        
+        
+    }
+}
+
+
+struct SearchView: View {
+    
+    @StateObject var viewModel: ViewModel
+    @Binding var city: String
+    
+    var body: some View {
+        VStack {
+            HStack{
+                TextField("City", text: $city)
+                
+                
+                Button {
+                    Task {
+                        await viewModel.getData(city: city)
+                        city = ""
+                    }
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .scaledToFit()
+                        .frame(width: 25, height: 25)
+                }
+            }
+            .padding(10)
+            .background(.cyan)
+            .cornerRadius(10)
+            .shadow(color: .black, radius: 10, x: 0, y: 10)
+        }
+    }
+}
+    
+struct TempView: View {
+    @StateObject var viewModel: ViewModel
+    var body: some View {
+        VStack {
+            
+            Text(viewModel.WeatherData?.name ?? "")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.yellow)
+            
+            
+            Text(String(format: "%.0f °C", viewModel.WeatherData?.main.temp ?? 0.0))
+                .font(.system(size: 50))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            
+            
+            Text(viewModel.WeatherData?.weather[0].main ?? "")
+                .foregroundColor(.white)
+                .font(.title2)
+            
+            
+        }
+        .padding(.top, 30)
+        .shadow(color: .black, radius: 10, x: 0, y: 10)
+        
+    }
+}
+
+struct BottomView: View {
+    @StateObject var viewModel: ViewModel
+    var body: some View {
+        VStack {
+            HStack(spacing: 40){
+                VStack(spacing: 10) {
+                    HStack {
+                        Image(systemName: "thermometer.low")
+                            .renderingMode(.original)
+                        
+                        Text(String(format: "%.0f °C", viewModel.WeatherData?.main.temp_min ?? 0.0))
+                            .font(.headline)
+                    }
+                    Text("Min temp")
+                }
+                .padding(.vertical)
+                
+                
+                
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "thermometer.high")
+                            .renderingMode(.original)
+                        
+                        Text(String(format: "%.0f °C", viewModel.WeatherData?.main.temp_max ?? 0.0))
+                            .font(.headline)
+                    }
+                    Text("Max temp")
+                }
+                
+                VStack {
+                    Text(String(format: "%.0f °C", viewModel.WeatherData?.main.feels_like ?? 0.0))
+                        .font(.headline)
+                    Text("Feels like")
+                }
+                
+            }
+            .padding()
+            .shadow(color: .black, radius: 10, x: 0, y: 10)
+         
+            
+            HStack(spacing: 40){
+                
+                
+                VStack {
+                    
+                    HStack {
+                        
+                        Image(systemName: "humidity.fill")
+                            .renderingMode(.original)
+                        
+                        Text("\(viewModel.WeatherData?.main.humidity ?? 0)")
+                            .font(.headline)
+                        
+                    }
+                    
+                    Text("Humidity")
+                }
+                
+                VStack {
+                    HStack {
+                        
+                        Image(systemName: "wind")
+                            .renderingMode(.original)
+                        
+                        Text(String(format: "%.2f", viewModel.WeatherData?.wind.speed ?? 0.0))
+                            .font(.headline)
+                    }
+                    Text("Wind Speed")
+                }
+                
+                VStack {
+                        Text("\(viewModel.WeatherData?.main.pressure ?? 0)")
+                            .font(.headline)
+                    
+                    Text("Pressure")
+                }
+                
+            }
+            .padding()
+            .shadow(color: .black, radius: 10, x: 0, y: 10)
+            
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
+
+
